d3aff0a2d5aea12196806ad17ed3108c270390b6
tpoisot <timothee.poisot@uqar.ca>
2013-06-24 14:43:08 -0400

    Added estimation of the power law parameter

--- sim_unipartite.py
+++ sim_unipartite.py
@@ -4,18 +4,31 @@ import scipy.stats as sps
 import matplotlib.pyplot as plt
 import networkx as nx
 
+fitfunc = lambda p, x: p[0] + p[1] * x
+errfunc = lambda p, x, y: y - fitfunc(p, x)
+
+def powerlaw(y):
+  s_y = np.sort(y)[::-1]
+  X = sp.log10(np.arange(1,len(s_y)+1))
+  Y = sp.log10(s_y)
+  pinit = [10.0, 0.0]
+  out = sp.optimize.leastsq(errfunc, pinit, args=(X, Y), full_output = 1)
+  index = out[0][1]
+  amp = 10**out[0][0]
+  return index
+
 def m(n):
   return n-1
 
 def M(n):
   return n*(n-1)/float(2) 
 
-n = 25
+n = 30
 step = 10
-repls = 50
+repls = 200
 
 test_links = np.arange(m(n)*2, M(n), step)
-store_out = np.zeros((len(test_links)*repls,6))
+store_out = np.zeros((len(test_links)*repls,7))
 index = 0
 for tl in test_links:
   print tl
@@ -28,6 +41,7 @@ for tl in test_links:
     result.append(np.var(d_overall))
     result.append(sps.stats.kurtosis(d_overall))
     result.append(sps.stats.skew(d_overall))
+    result.append(powerlaw(d_overall))
     store_out[index] = result
     index += 1
 
