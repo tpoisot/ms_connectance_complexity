bfb28bb275dc62a0b4c3289e2f44f9e4147d250a
tpoisot <timothee.poisot@uqar.ca>
2013-06-25 12:12:08 -0400

    Figures

--- sim_analytical.py
+++ sim_analytical.py
@@ -5,22 +5,17 @@ import scipy.misc as sc
 def M(n):
   return n*(n-1)/2
 
-def log_fac(n):
-  S = 0
-  for i in range(2,n+1): S += np.log(i)
-  return S
-
 def C(n, k):
   return sc.comb(n, k)
 
 def R(n,l):
   total = C(M(n), l)
   reali = total - n * C(M(n-1), l)
-  return np.array([l, total, reali])
+  return np.array([l/float(M(n)), total, reali])
 
 n = 10
 
-l_iter = xrange(2*n, M(n)+1)
+l_iter = xrange(n+2, M(n)+1)
 res_store = np.zeros((len(l_iter), 3))
 
 pos = 0
